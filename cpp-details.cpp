f42075f73730ceb179078a41472f335458360ec4
Sohail Siadat <sohale@gmail.com>
2018-08-31 18:33:48 +0100

    priority-queue

--- cpp-details.cpp
+++ cpp-details.cpp
@@ -4,6 +4,16 @@
 class my_comparator {
 }
 
+// priority_queue
+/*
+    A priority_queue does not need a separate field for the value (key) used for sorting. A brilliant way of abstracting: pop_back() returns the largest.
+    What is it all about?
+    ''its first element is always the greatest of the elements it contains, according to some strict weak ordering criterion``
+    Common use:
+    Internal: ?
+    You cannot iterate elements.
+    related: heap.
+*/
+
 
 // TODO: split into separate cpp files in a folder for this. `comparator.cpp`, etc.
-
