df5954ebd43237d0d7522d510bf63fc129af70f3
Sohail Siadat <sohale@gmail.com>
2019-03-09 19:26:21 +0000

    output

--- js/promise-a-tutorial/promise1.js
+++ js/promise-a-tutorial/promise1.js
@@ -37,3 +37,17 @@ const p1 = new Promise((resolve, reject)=>{
 });
 
 console.log('p1', p1);
+
+/*
+output:
+::this {}
+::this { q: 'this-q' }
+promise 1 execution
+this: W6 { q: 'this-q' }
+U7 undefined
+V8 undefined
+p1 Promise { <pending> }
+then t1= A2
+then: this: I3 { q: 'this-q' }
+*/
+
