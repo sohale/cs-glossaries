8c909f4247ef783a5a26d66b23e0df8a1ee8a069
Sohail Siadatnejad <sohail.siadatnejad@lloydsbanking.com>
2022-04-11 11:56:47 +0100

    simple char

--- cpp-practice/char8.cpp
+++ cpp-practice/char8.cpp
@@ -3,12 +3,18 @@
 
 #include <iostream>
 
+using std::endl;
 
 int main() {
     wchar_t frog = '🐸';
     // warning: multi-character character constant [-Wmultichar]
     std::cout << frog;
-    // output: -257978184%
+    // output: -257978184
+    std::cout << endl;
 
+    char frogc = '🐸';
+    // warning: warning: overflow in conversion from 'int' to 'char' changes value from '-257978184' to ''\37777777670'' [-Woverflow]
+    std::cout << frogc << endl;
+    // output: ?
 
 }
