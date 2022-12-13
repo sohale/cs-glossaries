519726b1eee2159cf691e365e24b0bd0f2194610
Sohail Siadat <sohale@gmail.com>
2022-12-13 15:52:16 +0000

    Create eigen-notes.cpp

---
+++ c++/eigen-notes.cpp
@@ -0,0 +1,42 @@
+
+
+ #include <Eigen/Eigen>
++// using Eigen::Matrix;
+
+  
+This is 
+
+internal definition:
+```C++
+typedef Matrix<float, 3, 1> Vector3f;
+```
+
+Does not work:
+```c++
+template<typename R>
+typedef Eigen::Matrix<R, 3, 1> vec3_tt;
+```
+Instead do this:
+```c++
+template<typename R>
+using vec3_tt = typename Eigen::Matrix<R, 3, 1>;
+```
+
+Instead of this (in fact this file just does this: `#include "Dense" #include "Sparse"`),
+```c++
+#include <Eigen/Eigen>
+using Eigen::Matrix;
+```
+instead, do this:
+```C++
+#include <Eigen/Dense>
+```
+
+```c++
+  Eigen::Matrix<float, 3, 1> v = Eigen::Matrix<float, 3, 1>::Zero();
+```
+
+Constants:
+```c++
+  Eigen::Matrix4d::Constant(4.5);
+```
